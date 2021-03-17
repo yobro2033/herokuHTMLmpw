cfc0fb116d0e848c4b5f4fa6c9ca5a7bdd5f1a7c
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-18 00:40:42 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return render_template('mainpage.html')
+
 @app.route('/first')
 def hello_world():
     return render_template('first.html')
