fca485baaf7d6c8640fad586db499df8749c3f81
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-18 00:40:12 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/first')
+def hello_world():
+    return render_template('first.html')
+
+@app.route('/login')
 def hello_world():
     return render_template('login.html')
-
